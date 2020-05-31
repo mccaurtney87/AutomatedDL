58f427a7fd5ddc54084b3f7b640b629cfea9d9ff
Rishabh Tripathi <trishabh8@gmail.com>
2020-05-31 09:40:40 +0530

    Changed modification path

--- modify.py
+++ modify.py
@@ -2,11 +2,10 @@ import os
 for file in os.listdir('/AutomatedDL/Churn-Model'):
     if file.endswith('.py'):
         myfile = file
-        print(file)
-
+        print(
 print('Modifying file : ',myfile )
 
-f = open(myfile,"r")
+f = open('Churn-Model/'+myfile,"r")
 filedata = f.read()
 f.close()
 
@@ -15,6 +14,6 @@ newdata = newdata.split("\n")
 newdata.insert(25,"model.add(Dense(units=16,activation='relu'))")
 newdata = "\n".join(newdata)
 
-f = open(myfile,"w")
+f = open('Churn-Model/'+myfile,"w")
 f.write(newdata)
 f.close()

75ba88616169fcc8e7a8281624356af7306ca2fa
Rishabh Tripathi <trishabh8@gmail.com>
2020-05-31 18:45:00 +0530

    .

--- Churn-Model/churn_modelling.py
+++ Churn-Model/churn_modelling.py
@@ -38,9 +38,9 @@ import os
 if accuracy > 0.85:
 
      print('accuracy is: ',accuracy*100)
-     os.system('curl --user admin:b@8788 http://192.168.43.57:8080/job/Mt3_job3/build?token=success')
+     os.system('curl --user admin:*** http://192.168.43.57:8080/job/Mt3_job3/build?token=success')
 
 else:
 
      print('accuracy is: ',accuracy*100)
-     os.system('curl --user admin:b@8788 http://192.168.43.57:8080/job/Mt3_job4/build?token=failure')
+     os.system('curl --user admin:*** http://192.168.43.57:8080/job/Mt3_job4/build?token=failure')
